e5b5ac31fe4a1d4b4ba77e9b44ac5bdf340e7107
GosuCode <gosucode1945@gmail.com>
2024-07-08 08:30:18 +0545

    add: gave functionality to calculator

--- app/src/main/java/com/example/learning/CalculatorActivity.java
+++ app/src/main/java/com/example/learning/CalculatorActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,30 +14,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CalculatorActivity extends AppCompatActivity {
 
+    EditText num1;
+    EditText num2;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calculator);
-        EditText num1 = findViewById(R.id.num1);
-        EditText num2 = findViewById(R.id.num2);
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        result = findViewById(R.id.result);
+
     }
 
     public void gotoHome(View view) {
         Intent back = new Intent(this, HomeScreen.class);
         startActivity(back);
     }
+
     public void addNum(View view) {
+        double number1 = Double.parseDouble(num1.getText().toString());
+        double number2 = Double.parseDouble(num2.getText().toString());
+        double sum = number1 + number2;
+        result.setText(String.valueOf(sum));
     }
 
     public void subNum(View view) {
+        double number1 = Double.parseDouble(num1.getText().toString());
+        double number2 = Double.parseDouble(num2.getText().toString());
+        double sub = number1 - number2;
+        result.setText(String.valueOf(sub));
     }
-
     public void divNum(View view) {
+        double number1 = Double.parseDouble(num1.getText().toString());
+        double number2 = Double.parseDouble(num2.getText().toString());
+        double div = number1 / number2;
+        result.setText(String.valueOf(div));
     }
 
     public void mulNum(View view) {
+        double number1 = Double.parseDouble(num1.getText().toString());
+        double number2 = Double.parseDouble(num2.getText().toString());
+        double mul = number1 * number2;
+        result.setText(String.valueOf(mul));
     }
 
 }
