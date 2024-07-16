3145776d972fa9353482e6647451755d0dfefbb0
GosuCode <gosucode1945@gmail.com>
2024-07-16 07:48:54 +0545

    learn: fragment and navigating to different fragment

--- app/src/main/java/com/example/learning/FragmentActivity.java
+++ app/src/main/java/com/example/learning/FragmentActivity.java
@@ -1,12 +1,17 @@
 package com.example.learning;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentActivity extends AppCompatActivity {
 
@@ -15,10 +20,28 @@ public class FragmentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_fragment);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button tab1 = findViewById(R.id.tab1);
+        Button tab2 = findViewById(R.id.tab2);
+
+        tab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager frg = getSupportFragmentManager();
+                FragmentTransaction frt = frg.beginTransaction();
+                frt.replace(R.id.fragmentContainerView, new FirstFragment());
+                frt.commit();
+            }
+        });
+
+        tab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager frm = getSupportFragmentManager();
+                FragmentTransaction frt = frm.beginTransaction();
+                frt.replace(R.id.fragmentContainerView, new SecondFragment());
+                frt.commit();
+            }
         });
     }
 }
